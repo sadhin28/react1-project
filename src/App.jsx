e7ab0752973f118ada6b5e8fabe51e6da120ca9c
sadhin28 <taosifsadhin113@gmail.com>
2025-04-28 00:31:02 +0600

    use arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,17 @@ function handelClick(){
 const handelClick2=()=>{
   alert('handel Click 2')
 }
+const addToFive = (number)=>{
+    alert(number + 5)
+}
   return (
     <>
      <h3>React Core Concept 2</h3>
      <button onClick={handelClick}>Click 1</button>
      <button onClick={handelClick2}>Click 2</button>
+     <button onClick={()=>{alert('click 3')}}>Click 3</button>
+     //parameter pass using arrow function
+     <button onClick={()=>addToFive(3)}>AddToFive</button>
     </>
   )
 }
