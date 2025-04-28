11acb4cd65b7d8c63006ac20de9f14bae5f29b68
sadhin28 <taosifsadhin113@gmail.com>
2025-04-28 23:28:25 +0600

    add trigaring

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -1,11 +1,12 @@
 import './friends.css'
 export default function Friends({friend}){
-   const {name,email}=friend;
+   const {name,email,id}=friend;
 
     return(
         <div className='box'>
             <h4>Name :{name} </h4>
             <p>Email : {email}</p>
+            <p>Id: {id}</p>
         </div>
     )
 }
