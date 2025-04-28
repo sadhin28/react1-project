e7d73c4eb76110ccbbeb0ca193110cb0dddbfd20
sadhin28 <taosifsadhin113@gmail.com>
2025-04-28 21:13:38 +0600

    add team

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
+
 export default function Team(){
+    const [team,setTeam]=useState(11);
+
+    const teamStyle={
+        border: '2px solid purple',
+        margin: '15px',
+        padding: '15px',
+        borderRadius : '15px'
+    }
     return(
-        <div>
-            <h3>Team:</h3>
+        <div style={teamStyle}>
+            <h3>Team : {team}</h3>
         </div>
     )
 }
