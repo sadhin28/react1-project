af69a068b500f3d06ca70ba6293f5a7e92cc1708
sadhin28 <taosifsadhin113@gmail.com>
2025-04-28 21:32:21 +0600

    add team

--- src/Team.jsx
+++ src/Team.jsx
@@ -2,7 +2,16 @@ import { useState } from "react"
 
 export default function Team(){
     const [team,setTeam]=useState(11);
-
+    const addHandler =()=>{
+        const newTeam = team +1;
+        setTeam(newTeam)
+       
+    }
+    const removeHandler =()=>{
+        const newTeam = team - 1;
+        setTeam(newTeam)
+       
+    }
     const teamStyle={
         border: '2px solid purple',
         margin: '15px',
@@ -12,6 +21,13 @@ export default function Team(){
     return(
         <div style={teamStyle}>
             <h3>Team : {team}</h3>
+           <div style={{
+            display:'flex',
+            justifyContent:'space-between'
+           }}>
+                <button onClick={addHandler}>Add</button>
+                <button onClick={removeHandler}>RemoveTeam</button>
+           </div>
         </div>
     )
 }
